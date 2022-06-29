38c5cdea6e1739ae997a520f8a38d0d44466d034
Yusaku Mandai <mandaiy@users.noreply.github.com>
2022-06-29 22:10:02 +0900

    Add z_cleanup fish function (#24)
    * Add z_cleanup fish function
    
    * Fix for POSIX sed
    
    * Fix config.fish

---
+++ fish/functions/z_cleanup.fish
@@ -0,0 +1,14 @@
+function z_cleanup --description "Clean up z data"
+    # Because POSIX sed does not have -i option,
+    # here we use tmpfile and redirect to update the file.
+    set -l tmp_file (mktemp)
+
+    for d in (cat $Z_DATA | awk -F"|" '{print $1}')
+        if not test -e $d
+            sed "\|^$d|d" $Z_DATA > $tmp_file
+            cp $tmp_file $Z_DATA
+        end
+    end
+
+    rm -f $tmp_file
+end
