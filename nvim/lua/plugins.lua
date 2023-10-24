61f1b80cca74153fd6110b926a60dc5f358d071f
Yusaku Mandai <yusaku.mandai@estie.co.jp>
2023-10-24 10:36:23 +0900

    nvim: add dbtpal plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -266,6 +266,41 @@ return require("packer").startup({
          end,
       })
 
+      -- A plugin for dbt.
+      use({
+         "PedramNavid/dbtpal",
+         config = function()
+            local dbt = require("dbtpal")
+            dbt.setup({
+               -- Path to the dbt executable
+               path_to_dbt = "dbt",
+
+               -- Path to the dbt project, if blank, will auto-detect
+               -- using currently open buffer for all sql,yml, and md files
+               path_to_dbt_project = vim.env.DBT_PROJECT_DIR,
+
+               -- Path to dbt profiles directory
+               path_to_dbt_profiles_dir = vim.env.DBT_PROFILES_DIR or vim.fn.expand("~/.dbt"),
+
+               -- Search for ref/source files in macros and models folders
+               extended_path_search = true,
+
+               -- Prevent modifying sql files in target/(compiled|run) folders
+               protect_compiled_files = true,
+            })
+
+            -- Setup key mappings
+            -- vim.keymap.set('n', '<leader>drf', dbt.run)
+            -- vim.keymap.set('n', '<leader>drp', dbt.run_all)
+            -- vim.keymap.set('n', '<leader>dtf', dbt.test)
+            vim.keymap.set("n", "<leader>dm", require("dbtpal.telescope").dbt_picker)
+
+            -- Enable Telescope Extension
+            require("telescope").load_extension("dbtpal")
+         end,
+         requires = { { "nvim-lua/plenary.nvim" }, { "nvim-telescope/telescope.nvim" } },
+      })
+
       -- Handles extra whitespaces.
       use({
          "ntpeters/vim-better-whitespace",
